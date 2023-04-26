228c3b4447fcc64b11cd66de5a3ee21c42f07ffe
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-04-26 14:56:27 +0200

    🚀 feat(models.py): add Recipe model with fields for title, description, author, image, ingredients, instructions, created_at, and updated_at. Add __str__ method to return title.

--- recipegram_fullstack/recipes/models.py
+++ recipegram_fullstack/recipes/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Recipe(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="recipes/", null=True, blank=True)
+    ingredients = models.ListField(models.CharField(max_length=255))
+    instructions = models.ListField(models.CharField(max_length=255))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
