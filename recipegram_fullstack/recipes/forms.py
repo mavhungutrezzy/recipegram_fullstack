78d882b338c19afb916a76f0440b27ee4f7534c7
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-04-26 16:13:12 +0200

    🚀 feat(recipes): add tags field to Recipe model 🚀 feat(recipes): add tags choices to Recipe model 🚀 feat(recipes): add tags field to RecipeForm 🚀 feat(recipes): add tags choices to RecipeForm 🚀 feat(recipes): add migration to alter Recipe model and add tags field

--- recipegram_fullstack/recipes/forms.py
+++ recipegram_fullstack/recipes/forms.py
@@ -6,8 +6,16 @@ from .models import Recipe
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = ["title", "description", "image", "ingredients", "instructions"]
+        fields = [
+            "title",
+            "description",
+            "image",
+            "ingredients",
+            "instructions",
+            "tags",
+        ]
 
     image = forms.ImageField(required=False)
     ingredients = forms.CharField(widget=forms.Textarea)
     instructions = forms.CharField(widget=forms.Textarea)
+    tags = forms.ChoiceField(choices=Recipe.TAGS_CHOICES)
--- recipegram_fullstack/recipes/models.py
+++ recipegram_fullstack/recipes/models.py
@@ -5,6 +5,12 @@ from django.db import models
 
 User = get_user_model()
 
+TAGS_CHOICES = (
+    ("breakfast", "Breakfast"),
+    ("lunch", "Lunch"),
+    ("dinner", "Dinner"),
+)
+
 
 class Recipe(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -14,11 +20,12 @@ class Recipe(models.Model):
     image = models.ImageField(upload_to="recipes/", null=True, blank=True)
     ingredients = models.JSONField()
     instructions = models.JSONField()
+    tags = models.CharField(max_length=255, choices=TAGS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ("-created_at",)
-        
+
     def __str__(self):
         return self.title
