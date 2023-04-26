7020fdb1c47aae865e3c7ec23bc089fa1c1522a9
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-04-26 16:07:37 +0200

    🚀 feat(favorites): add index view to display all favorites 🐛 fix(favorites): import required modules in views.py

--- recipegram_fullstack/favorites/views.py
+++ recipegram_fullstack/favorites/views.py
@@ -1,3 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .models import Favorite
+from recipes.models import Recipe
+
+User = get_user_model()
+
+
+def index(request):
+    favorites = Favorite.objects.all().order_by("-created_at")
+    paginator = Paginator(favorites, 6)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "favorites/index.html", {"page_obj": page_obj})
+
