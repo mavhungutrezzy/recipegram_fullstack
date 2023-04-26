5a24052c4117aeca39e1c975cf300340efd79aba
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-04-26 15:01:40 +0200

    🔀 merge(models.py): merge two models.py files into one ✨ feat(models.py): add Favorite and Follow models to the file 🔧 chore(models.py): remove unnecessary comment from the file

--- recipegram_fullstack/favorites/models.py
+++ recipegram_fullstack/favorites/models.py
@@ -1,3 +1,19 @@
+import uuid
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Favorite(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    recipe = models.ForeignKey("recipes.Recipe", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("recipe", "user")
+
+    def __str__(self):
+        return str(self.recipe)
--- recipegram_fullstack/follows/models.py
+++ recipegram_fullstack/follows/models.py
@@ -1,3 +1,19 @@
+import uuid
+
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Follow(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    follower = models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("follower", "following")
+
+    def __str__(self):
+        return str(self.follower)
