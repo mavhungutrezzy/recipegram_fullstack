9440a10fb21519dd672ceb03b3972e62397a3e7c
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-04-27 20:14:21 +0200

    🔥 chore(urls.py): remove home page URL from urlpatterns ✨ feat(urls.py): add root URL to recipes app 🎨 style(forms.py): improve form layout and add labels 🐛 fix(views.py): initialize form with instance in edit view ✨ feat(templates/base.html): add Bootstrap Icons CDN ✨ feat(templates/base.html): add create recipe link to navbar ✨ feat(templates/recipes/create.html): create recipe form template ✨ feat(templates/recipes/edit.html): update recipe form template
    🎨 style(index.html): add meal type buttons and author name with person icon to recipe cards
    🐛 fix(index.html): fix recipe title link to detail view

--- recipegram_fullstack/recipes/forms.py
+++ recipegram_fullstack/recipes/forms.py
@@ -15,12 +15,56 @@ class RecipeForm(forms.ModelForm):
             "tags",
         ]
 
-    image = forms.ImageField(required=False)
-    ingredients = forms.CharField(widget=forms.Textarea)
-    instructions = forms.CharField(widget=forms.Textarea)
-    servings = forms.IntegerField(min_value=1)
-    preparation_time = forms.IntegerField(min_value=0)
-    cook_time = forms.IntegerField(min_value=0)
-    total_time = forms.IntegerField(min_value=0)
-    tags = forms.ChoiceField(choices=TAGS_CHOICES)
-    meal_type = forms.ChoiceField(choices=MEAL_TYPE_CHOICES)
+        labels = {
+            "title": "Recipe Title",
+            "description": "Description",
+            "image": "Image",
+            "ingredients": "Ingredients",
+            "instructions": "Instructions",
+            "tags": "Tags",
+        }
+
+        widgets = {
+            "title": forms.TextInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control"}),
+            "image": forms.ClearableFileInput(attrs={"class": "form-control"}),
+            "ingredients": forms.Textarea(attrs={"class": "form-control"}),
+            "instructions": forms.Textarea(attrs={"class": "form-control"}),
+            "tags": forms.Select(attrs={"class": "form-control"}),
+        }
+
+    servings = forms.IntegerField(
+        label="Servings",
+        min_value=1,
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+    )
+
+    preparation_time = forms.IntegerField(
+        label="Preparation Time (in minutes)",
+        min_value=0,
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+    )
+
+    cook_time = forms.IntegerField(
+        label="Cook Time (in minutes)",
+        min_value=0,
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+    )
+
+    total_time = forms.IntegerField(
+        label="Total Time (in minutes)",
+        min_value=0,
+        widget=forms.NumberInput(attrs={"class": "form-control"}),
+    )
+
+    meal_type = forms.ChoiceField(
+        label="Meal Type",
+        choices=MEAL_TYPE_CHOICES,
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
+
+    tags = forms.ChoiceField(
+        label="Tags",
+        choices=TAGS_CHOICES,
+        widget=forms.Select(attrs={"class": "form-control"}),
+    )
