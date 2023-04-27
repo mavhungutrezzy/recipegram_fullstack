fb08b753fffa12b1d5068eac77315dbef7c43e15
mavhungutrezzy <mavhungutrezzy@gmail.com>
2023-04-27 18:31:41 +0200

    🚀 feat(admin.py): add tags and meal_type to RecipeAdmin list_display 🚀 feat(forms.py): add servings, preparation_time, cook_time, total_time, and meal_type fields to RecipeForm 🚀 feat(models.py): add servings, meal_type, preparation_time, cook_time, and total_time fields to Recipe model 🚀 feat(project.css): add recipe-title and recipe-badge classes
    💄 style(index.html): add class to recipe title and recipe badge to improve styling
    🔧 chore(index.html): add cook time and meal type to recipe card to improve user experience

--- recipegram_fullstack/recipes/admin.py
+++ recipegram_fullstack/recipes/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
+
 from .models import Recipe
 
 
 class RecipeAdmin(admin.ModelAdmin):
-    list_display = ("title", "author")
-    
+    list_display = ("title", "author", "tags", "meal_type")
+
     def save_model(self, request, obj, form, change):
         obj.author = request.user
         super().save_model(request, obj, form, change)
-        
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         return qs if request.user.is_superuser else qs.filter(author=request.user)
-    
-    
+
 
 admin.site.register(Recipe, RecipeAdmin)
--- recipegram_fullstack/recipes/forms.py
+++ recipegram_fullstack/recipes/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import TAGS_CHOICES, Recipe
+from .models import MEAL_TYPE_CHOICES, TAGS_CHOICES, Recipe
 
 
 class RecipeForm(forms.ModelForm):
@@ -18,4 +18,9 @@ class RecipeForm(forms.ModelForm):
     image = forms.ImageField(required=False)
     ingredients = forms.CharField(widget=forms.Textarea)
     instructions = forms.CharField(widget=forms.Textarea)
+    servings = forms.IntegerField(min_value=1)
+    preparation_time = forms.IntegerField(min_value=0)
+    cook_time = forms.IntegerField(min_value=0)
+    total_time = forms.IntegerField(min_value=0)
     tags = forms.ChoiceField(choices=TAGS_CHOICES)
+    meal_type = forms.ChoiceField(choices=MEAL_TYPE_CHOICES)
